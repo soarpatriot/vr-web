649462d79ea7983c0432ac952d790b59d17feebd
Soar <85624529@qq.com>
2017-03-24 22:25:12 +0800

    change the prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://api.sanweiyun.com"'
+  API_URL: '"http://api.sanweiyun.com"',
   ASSETS_URL: '"http://assets.dreamreality.cn"'
 }
