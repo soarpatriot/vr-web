b4268e8699df8eb1443a3c085fffbacaaf1b574e
Soar <85624529@qq.com>
2017-11-15 11:40:33 +0800

    change assets to https

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: '"http://www.sanweiyun.com"',
   NODE_ENV: '"production"',
   API_URL: '"http://api.sanweiyun.com"',
-  ASSETS_URL: '"http://assets.dreamreality.cn"'
+  ASSETS_URL: '"https://assets.dreamreality.cn"'
 }
